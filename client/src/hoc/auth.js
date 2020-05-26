fb4b3d1bab60dab58d33444fd2d9672bc8e70821
dbstjrwnekd <sh4869@ajou.ac.kr>
2020-05-26 18:43:54 +0900

    서버 속도 개선 2

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -15,14 +15,15 @@ export default function(SpecificComponent, option, adminRoute = null){
         
         //back에 req날리기
         useEffect(() => {
-           dispatch(auth()).then(res => {
-               //console.log("hoc에서 묻는다", res);
-               setVerify(res.payload.verify);
-           });
-           return () => {
-               setVerify(false);
-           }
-        }, [verify]);
+            async function fetchData() {
+                await dispatch(auth()).then(res => {
+                    //console.log("hoc에서 묻는다", res);
+                    setVerify(res.payload.verify);
+                });
+            }
+           
+            fetchData();
+        }, []);
 
         return option==null? <SpecificComponent/> :option? verify? <SpecificComponent/> : <LoginPage/> : verify? <LoginPage/> : <SpecificComponent/>
 
